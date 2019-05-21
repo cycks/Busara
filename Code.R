672e782fad6c8530f5c32c0a16ad8928274add35
sikolia <sikolia.wycliffe@gmail.com>
2019-05-21 17:05:46 +0300

    Add visualizations for comparing means across the end_state of continous independent variables

--- Code.R
+++ Code.R
@@ -23,10 +23,31 @@ Fundraising <- read_csv("DS_Data Task_Fundraising.csv") %>%
   dplyr::mutate(launch_time = as.Date(launch_time, format = '%m/%d/%Y')) %>%
   dplyr::mutate_if(is.character, as.factor) %>%
   dplyr::mutate(time_diff = deadline_date - launch_time) %>%
-  dplyr::select(-c(launch_time, deadline_date)) %>%
+  dplyr::select(-c(launch_time, deadline_date, id)) %>%
   dplyr::mutate(category = as.numeric( factor(category) ) -1) %>%
   dplyr::mutate(currency = as.numeric( factor(currency) ) -1) %>%
   dplyr::mutate(country_location = as.numeric( factor(country_location) ) -1)
-  
-str(Fundraising)
-table(Fundraising$category)
+
+checking <- Fundraising %>%
+  dplyr::select(c("goal_amount", "amount_pledged", "end_state", "num_backers",
+                  "time_diff")) %>%
+  dplyr::group_by(., end_state) %>%
+  dplyr::summarise_all("mean")
+View(checking)
+
+ggplot(checking, aes(x=reorder(end_state, -goal_amount), y=goal_amount, fill=end_state)) +
+  geom_bar(stat = "identity")
+
+
+ggplot(checking, aes(x=reorder(end_state, -amount_pledged), y=amount_pledged, 
+                     fill=end_state)) +
+  geom_bar(stat = "identity")
+
+
+ggplot(checking, aes(x=reorder(end_state, -num_backers), y=num_backers, fill=end_state)) +
+  geom_bar(stat = "identity")
+
+ggplot(checking, aes(x=reorder(end_state, -time_diff), y=time_diff, fill=end_state)) +
+  geom_bar(stat = "identity")
+
+
